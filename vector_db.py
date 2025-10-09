7c716a91e2771c339badb578d0529ded47f3d97d
amaliecjensen <amaliecolstrup@icloud.com>
2025-10-09 17:18:11 +0200

    started setup chroma db

---
+++ vector_db.py
@@ -0,0 +1,24 @@
+#husk at give persist_directory for at min chromadb ikke skal køre in-memory mode
+import chromadb
+
+def setup_database():
+    client = chromadb.PersistentClient("./chroma_db") #opretter database i mappen
+    collection = client.get_or_create_collection(
+        name="email"
+    )
+    return client, collection
+
+client, collection = setup_database()
+
+def save_emails(sender, subject, body, isImportant):
+
+    data = {
+        ids=[],
+        embeddings=[],
+        metadatas=[],
+        documents=[]
+    }
+
+    collection.add()
+    print(f"saved data to DB")
+    
